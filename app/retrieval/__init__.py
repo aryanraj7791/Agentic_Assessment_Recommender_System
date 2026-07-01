32526b4cb03f413a9d6624d1e03b6aba3f0df8a4
Aryan Raj <aryanraj5371@gmail.com>
2026-07-02 01:32:33 +0530

    Remove eager Chroma imports

--- app/retrieval/__init__.py
+++ app/retrieval/__init__.py
@@ -1,7 +1,3 @@
 """Retrieval package."""
 
-from app.retrieval.chroma_store import ChromaStore
-from app.retrieval.embeddings import EmbeddingService
-from app.retrieval.reranker import Reranker
-
-__all__ = ["ChromaStore", "EmbeddingService", "Reranker"]
+__all__ = []
